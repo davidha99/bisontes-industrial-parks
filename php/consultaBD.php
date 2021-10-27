97741f6b58f2366cfdde49d750e8e98f9071cf07
mauriciosanchezmusic <61812923+mauriciosanchezmusic@users.noreply.github.com>
2021-10-26 20:25:19 -0500

    php
    Archivo de conexion con apache

---
+++ php/consultaBD.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+define("cServidor","localhost");
+define("cUsuario","bisontes");
+define("cClave","bisontes");
+define("cBd","bisontes");
+
+function consultaBD($consulta){
+$conexion = mysqli_connect(cServidor,cUsuario,cClave,cBd);
+global $resultado;
+$resultado = mysqli_query($conexion,$consulta);
+
+if($resultado){
+mysqli_close($conexion);
+return $resultado;
+}
+else{
+mysqli_close($conexion);
+return FALSE;
+}
+
+}
+
+?>
