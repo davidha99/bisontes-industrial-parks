f7c1e43ea8058b901ec47123c398b987e85a41d7
StrictName <62078976+StrictName@users.noreply.github.com>
2021-10-29 11:57:43 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset=UTF-8>
+    <title>datos</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div id="div__bisontes-logo">
+            <img src="img/logo.png" alt="Bisontes Industrial Park Logo" id="bisontes-logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+                <li><a href="historia.html">Historia</a></li>
+                <li><a href="multimedia.html">Multimedia</a></li>
+                <li><a href="registro.html">Registro</a></li>
+                <li><a href="informacion.html">Información</a></li>
+            </ul>
+        </nav>
+    </header>
+
+<?php
+
+    include('php/consultaBD.php');
+	$nombreEmp=$_POST['nombreEmp'];
+	$telefono=$_POST['telefono'];
+	$correo=$_POST['correo'];
+	$industria=$_POST['industria'];
+	
+	$consulta = "INSERT INTO cliente (empresaNom, telefono, correo, industria) VALUES ('$nombreEmp','$telefono','$correo','$industria')";
+    
+	if(ConsultaBD($consulta)){
+        echo "Los siguientes datos han sido guardados:<br>";
+        echo "Nombre Empresa: ".$nombreEmp."<br>";
+        echo "Teléfono: ".$telefono."<br>";
+        echo "Correo: ".$email."<br>";
+        echo "Industria: ".$mensaje."<br>";
+	}
+	else
+		echo "Error al guardar los datos";
+
+?>
+
+</body>
+</html>
